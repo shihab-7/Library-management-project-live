71b1b39215ccc96027537437a6f921ceb4d89915
shihab <shihabshahriar543@gmail.com>
2024-01-07 07:17:37 +0600

    settings.py updated

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -33,6 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://library-management-project-6cy5.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
